79c9f40536e71ea7f6f665a29989322cd3885a1d
Denis Tome <D.Tome@cs.ucl.ac.uk>
2017-02-27 10:34:00 +0000

    Inlcuding computed results with predictions and error for papaer

--- test_net.sh
+++ test_net.sh
@@ -45,25 +45,32 @@
 # -o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
 # --num_parts 5 \
 # --run_part 3
+
+# python models/cpm_architecture/runCaffeTest.py \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
+# -o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
+# --num_parts 5 \
+# --run_part 4
+# python models/cpm_architecture/runCaffeTest.py \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
+# -o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
+# --num_parts 5 \
+# --run_part 5
+# python models/cpm_architecture/runCaffeTest.py \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
+# --merge_parts_dir models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
+#        --num_parts 5 \
+# python models/cpm_architecture/runCaffeTest.py \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
+# models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
+# --merge_parts_dir /home/denitome/Desktop/tmp/ \
+# --num_parts 5 \
+
 python models/cpm_architecture/runCaffeTest.py \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
--o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
---num_parts 5 \
---run_part 4
-python models/cpm_architecture/runCaffeTest.py \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
--o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
---num_parts 5 \
---run_part 5
-python models/cpm_architecture/runCaffeTest.py \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
---merge_parts_dir models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/ \
-       --num_parts 5 \
-python models/cpm_architecture/runCaffeTest.py \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_deploy_stg4.prototxt \
-models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test_tmp/pose_iter_22000.caffemodel \
---merge_parts_dir /home/denitome/Desktop/tmp/ \
---num_parts 5 \
+models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test/pose_deploy.prototxt \
+models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test/pose_iter_22000.caffemodel \
+--json_file models/cpm_architecture/jsonDatasets/H36M_annotations_testSet_yasin.json
+-o models/cpm_architecture/prototxt/caffemodel/manifold_diffarch3/to_test/yasin_pred/ \
